1c97ab7551a586ddc5771778d89f81c0614a07f7
Shanker joshi <shankerjoshi743@gmail.com>
2024-07-25 16:54:28 +0530

    some data added in file

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const gitData = {
+  "login": "sjcoder743",
+  "id": 121203972,
+  "node_id": "U_kgDOBzltBA",
+  "avatar_url": "https://avatars.githubusercontent.com/u/121203972?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/sjcoder743",
+  "html_url": "https://github.com/sjcoder743",
+  "followers_url": "https://api.github.com/users/sjcoder743/followers",
+  "following_url": "https://api.github.com/users/sjcoder743/following{/other_user}",
+  "gists_url": "https://api.github.com/users/sjcoder743/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/sjcoder743/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/sjcoder743/subscriptions",
+  "organizations_url": "https://api.github.com/users/sjcoder743/orgs",
+  "repos_url": "https://api.github.com/users/sjcoder743/repos",
+  "events_url": "https://api.github.com/users/sjcoder743/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/sjcoder743/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Shanker Joshi",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": "\"Code is like humor. When you have to explain it, it's not funny anymore.\" - Unknown | CS Student | Coding for fun and learning\r\n\r\n",
+  "twitter_username": null,
+  "public_repos": 22,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 1,
+  "created_at": "2022-12-21T23:42:25Z",
+  "updated_at": "2024-07-04T15:05:10Z"
+}
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -21,6 +56,10 @@ app.get("/insta", (req, res) => {
 
 app.get("/whatsapp", (req, res) => {
   res.send("<h1><i>Just looking for whatsApp</i></h1>")
+})
+
+app.get('/git', (req, res) => {
+  res.json(gitData)
 })
 
 app.listen(process.env.PORT, () => {
